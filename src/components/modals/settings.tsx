c3991b9b200b39d048cb3c83f20bb2b07e7613f0
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-09-28 14:36:28 +1000

    wip settings

--- src/components/modals/settings.tsx
+++ src/components/modals/settings.tsx
@@ -1,25 +1,67 @@
 import ReactModal from "react-modal";
 
 import "./settings.css";
+
+// import { LoginStore } from "../../lib/loginStore";
+// import { useLocation } from "wouter";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { LoginStore } from "../../lib/loginStore";
 import { useLocation } from "wouter";
+import styled from "styled-components";
+import { useProfile } from "../../lib/hooks";
+
+const LabeledField = styled.div`
+	background-color: var(--background-secondary);
+	margin: 20px;
+	padding: 10px;
+	display: flex;
+	justify-content: space-between;
+`
 
 const Settings = ({ close }: { close: () => unknown }) => {
 	const [, setLocation] = useLocation();
 
-	return (
-		<div>
-			<button onClick={close}>Close</button>
+	const logout = () => {
+		LoginStore.save(null);
+		setLocation("/login");
+	};
 
-			<button
-				onClick={() => {
-					LoginStore.save(null);
-					setLocation("/login");
-				}}
-			>
-				logout
-			</button>
-		</div>
+	const user = useProfile();
+
+	if (!user) return null;
+
+	return (
+		<Tabs>
+			<TabList>
+				<Tab>Profile</Tab>
+				<Tab onClick={logout}>Log out</Tab>
+			</TabList>
+
+			<TabPanel>
+				{/* Profile */}
+				<h1>Profile</h1>
+
+				<LabeledField>
+					<label>Email</label>
+					<input value={user.email} />
+				</LabeledField>
+
+				<LabeledField>
+					<label>Username</label>
+					<input value={user.name} />
+				</LabeledField>
+
+				<LabeledField>
+					<label>Display Name</label>
+					<input value={user.display_name} />
+				</LabeledField>
+
+				<LabeledField>
+					<label>Summary</label>
+					<input value={user.summary} />
+				</LabeledField>
+			</TabPanel>
+		</Tabs>
 	);
 };
 
