6c087d2d47f490de6203f9dd1f868a325f6ff9b7
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-12-16 20:41:52 +1100

    fix: ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore node global
+const BASE_PATH = process.env.BASE_PATH;
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react({ tsDecorators: true })],
+    base: BASE_PATH ?? undefined
 });
