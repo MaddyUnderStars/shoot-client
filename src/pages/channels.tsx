7684740eb7fa012afda6edd5f41df39e9630c330
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-07-23 18:05:25 +1000

    fix: hide the members list on mobile until I have working multiple sidebars

--- src/pages/channels.tsx
+++ src/pages/channels.tsx
@@ -3,9 +3,11 @@ import { ChatPane } from "@/components/chat-pane";
 import { MemberList } from "@/components/member-list";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { useChannel } from "@/hooks/use-channel";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export const ChannelsPageComponent = () => {
 	const channel = useChannel();
+	const isMobile = useIsMobile();
 
 	return (
 		<SidebarProvider
@@ -19,7 +21,7 @@ export const ChannelsPageComponent = () => {
 
 			<ChatPane />
 
-			{!channel ? null : <MemberList channel={channel} />}
+			{!channel || isMobile ? null : <MemberList channel={channel} />}
 		</SidebarProvider>
 	);
 };
