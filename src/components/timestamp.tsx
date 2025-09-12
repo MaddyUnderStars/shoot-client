e7ba432edf729b95af0c05a369595b599d11c708
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-09-12 16:09:53 +1000

    fix: don't show seconds in timestamp and hide date in small viewports

--- src/components/timestamp.tsx
+++ src/components/timestamp.tsx
@@ -1,12 +1,16 @@
+import { useIsMobile } from "@/hooks/use-mobile";
+
 const TIMESTAMP_LIMIT = 12 * 60 * 60 * 1000; // 12 hours
 
 export const Timestamp = ({ date }: { date: Date }) => {
+	const isMobile = useIsMobile();
+
 	if (Number.isNaN(date.getTime())) return null;
 
 	return (
 		<span>
-			{date.toLocaleTimeString()}
-			{Date.now() - date.valueOf() > TIMESTAMP_LIMIT
+			{date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+			{!isMobile && Date.now() - date.valueOf() > TIMESTAMP_LIMIT
 				? ` - ${date.toLocaleDateString()}`
 				: null}
 		</span>
