9d44de306b91d1938bf25c4da74ff8910429f73c
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-09-26 09:44:45 +0000

    feat: slightly nicer notifs

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,8 +4,18 @@
 self.addEventListener("push", (event) => {
 	if (!event.data) return;
 
+	const data = event.data.json();
+
 	const promise = self.registration.showNotification(
-		event.data?.text(),
+		data.title,
+		{
+			requireInteraction: true,
+			body: data.body,
+			image: data.image,
+			icon: null,
+			badge: null,
+			timestamp: data.sent,
+		}
 	);
 
 	event.waitUntil(promise);
