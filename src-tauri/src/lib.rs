ef1ce27ecd4bd8348ea55f3c21d6313ffae37cc8
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-05-09 16:17:59 +1000

    feat: migrate shoot-tauri into shoot-client

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,21 @@
+use tauri::Manager;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
+        .plugin(tauri_plugin_notifications::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
