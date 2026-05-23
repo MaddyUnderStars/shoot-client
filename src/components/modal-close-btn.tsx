e18af2541b547d7dda20e4d147ab39b90debce1c
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-05-23 21:25:42 +1000

    fix: set cursor-pointer properly

--- src/components/modal-close-btn.tsx
+++ src/components/modal-close-btn.tsx
@@ -1,18 +1,20 @@
 import { useModal } from "@ebay/nice-modal-react";
 import { XIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 export const ModalCloseButton = () => {
 	const modal = useModal();
 
 	return (
-		<button
-			className="pointer-events-auto cursor-pointer"
+		<Button
+			className="pointer-events-auto"
+			variant="outline"
 			type="button"
 			title="Close"
 			onClick={modal.remove}
 			onKeyDown={modal.remove}
 		>
 			<XIcon />
-		</button>
+		</Button>
 	);
 };
