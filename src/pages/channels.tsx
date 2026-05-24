ac093d8aa958db7560d231ea251f63ee75fc50a9
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-05-24 11:44:47 +1000

    fix: keep sidebar open when navigating. close #21

--- src/pages/channels.tsx
+++ src/pages/channels.tsx
@@ -1,32 +1,15 @@
-import { SwipeBarProvider } from "@luciodale/swipe-bar";
 import { ChatPane } from "@/components/chat-pane";
 import { MemberList } from "@/components/member-list";
-import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { useChannel } from "@/hooks/use-channel";
 
 export const ChannelsPageComponent = () => {
 	const channel = useChannel();
 
 	return (
-		<SwipeBarProvider
-			edgeActivationWidthPx={window.innerWidth / 3}
-			sidebarWidthPx={window.innerWidth}
-		>
-			<SidebarProvider
-				style={
-					// oxlint-disable-next-line typescript/no-unsafe-type-assertion
-					{
-						"--sidebar-width": "300px",
-					} as React.CSSProperties
-				}
-			>
-				<AppSidebar />
+		<>
+			<ChatPane />
 
-				<ChatPane />
-
-				{!channel ? null : <MemberList channel={channel} />}
-			</SidebarProvider>
-		</SwipeBarProvider>
+			{!channel ? null : <MemberList channel={channel} />}
+		</>
 	);
 };
