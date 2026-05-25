4e9e5524c4991b856e973d3219c20de40ad35294
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-05-25 19:59:51 +1000

    fix: chat box jumping when entering text

--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -125,7 +125,7 @@ const ChatSlateEditor = ({
 		<Slate editor={editor} initialValue={initialValue}>
 			<Editable
 				className={cn(
-					"placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input min-h-9 w-full max-w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none md:text-sm",
+					"placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input w-full max-w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none md:text-sm",
 					"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
 					"rounded-r-none border-r-0",
 					hasAttached ? "rounded-t-none" : "",
