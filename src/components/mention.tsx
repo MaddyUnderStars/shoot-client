35cbb107cf6c93f2bb1aa4744b5683e3a45e2b8e
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-07-04 19:51:41 +1000

    fix: do not remove @ if mentioned user does not exist

--- src/components/mention.tsx
+++ src/components/mention.tsx
@@ -4,7 +4,7 @@ import type { ActorMention } from "@/lib/client/common/actor";
 export const Mention = ({ user: user_id }: { user: ActorMention }) => {
 	const { user } = useUser(user_id);
 
-	if (!user) return user_id;
+	if (!user) return `@${user_id}`;
 
 	return <span className="underline text-link bg-secondary font-bold">@{user_id}</span>;
 };
