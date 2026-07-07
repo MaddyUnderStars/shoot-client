0c8a33ba0aa881a360148764f70eae190b849a61
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-07-07 21:23:52 +1000

    feat: create channel btn in channel settings

--- src/components/settings/channel-settings.tsx
+++ src/components/settings/channel-settings.tsx
@@ -4,6 +4,8 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { Button } from "../ui/button";
 import { getHttpClient } from "@/lib/http/client";
 import type { ActorMention } from "@/lib/client/common/actor";
+import NiceModal from "@ebay/nice-modal-react";
+import { CreateChannelModal } from "../modal/create-channel-modal";
 
 export const ChannelSettings = observer(() => {
 	const guild = useGuild();
@@ -25,29 +27,41 @@ export const ChannelSettings = observer(() => {
 	const channels = guild.channels;
 
 	return (
-		<Table>
-			<TableHeader>
-				<TableRow>
-					<TableHead>Name</TableHead>
-					<TableHead></TableHead>
-				</TableRow>
-			</TableHeader>
+		<div>
+			<Button
+				onClick={() => {
+					void NiceModal.show(CreateChannelModal, {
+						guild,
+					});
+				}}
+			>
+				Create Channel
+			</Button>
 
-			<TableBody>
-				{channels.map((channel) => (
-					<TableRow key={channel.mention}>
-						<TableCell>{channel.name}</TableCell>
-						<TableCell className="text-right">
-							<Button
-								variant="destructive"
-								onClick={deleteChannel.bind(null, channel.mention)}
-							>
-								Delete
-							</Button>
-						</TableCell>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Name</TableHead>
+						<TableHead></TableHead>
 					</TableRow>
-				))}
-			</TableBody>
-		</Table>
+				</TableHeader>
+
+				<TableBody>
+					{channels.map((channel) => (
+						<TableRow key={channel.mention}>
+							<TableCell>{channel.name}</TableCell>
+							<TableCell className="text-right">
+								<Button
+									variant="destructive"
+									onClick={deleteChannel.bind(null, channel.mention)}
+								>
+									Delete
+								</Button>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</div>
 	);
 });
